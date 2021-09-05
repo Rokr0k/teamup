5f41378c25e3115948c6ceb4d570ce96c2591bc0
Rokr0k <yangeh2225@gmail.com>
2021-09-05 22:12:13 +0900

    add gacha video

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 var start = document.getElementById("start");
-var length = document.getElementById("length");
-var except = document.getElementById("except");
-var count = document.getElementById("count");
-var run = document.getElementById("run");
-var container = document.getElementById("container");
+const length = document.getElementById("length");
+const except = document.getElementById("except");
+const count = document.getElementById("count");
+const run = document.getElementById("run");
+const container = document.getElementById("container");
+const wish = document.getElementById("wish");
+const chang = document.getElementById("chang");
+const animation = document.getElementById("animation");
 
 run.addEventListener("click", () => {
-    var array = new Array();
-    var exceptions = except.value.split(" ").map(v => +v).filter(v => typeof (v) == "number");
-    for (var i = +start.value; i < +start.value + +length.value; i++) {
-        var done = false;
-        for (var j = 0; j < exceptions.length; j++) {
+    if (animation.checked) {
+        wish.removeAttribute("hidden");
+        wish.play();
+    }
+    else {
+        work();
+    }
+});
+
+wish.addEventListener("ended", () => {
+    wish.setAttribute("hidden", "");
+    work();
+});
+
+function work() {
+    const array = new Array();
+    const exceptions = except.value.split(" ").map(v => +v).filter(v => typeof (v) == "number");
+    for (let i = +start.value; i < +start.value + +length.value; i++) {
+        let done = false;
+        for (let j = 0; j < exceptions.length; j++) {
             if (i == exceptions[j]) {
                 done = true;
                 break;
@@ -20,26 +38,40 @@ run.addEventListener("click", () => {
             array.push(i);
         }
     }
-    var teams = new Array(+count.value);
-    for (var i = 0; i < +count.value; i++) {
+    const teams = new Array(+count.value);
+    for (let i = 0; i < +count.value; i++) {
         teams[i] = new Array();
     }
-    var team = 0;
+    let team = 0;
     while (array.length > 0) {
-        var index = Math.floor(Math.random() * array.length);
+        const index = Math.floor(Math.random() * array.length);
         teams[team].push(array[index]);
         team = (team + 1) % (+count.value);
         array.splice(index, 1);
     }
-    for (var i = 0; i < teams.length; i++) {
+    for (let i = 0; i < teams.length; i++) {
         teams[i] = teams[i].sort((a, b) => a - b);
     }
     container.innerHTML = "";
-    for (var i = 0; i < teams.length; i++) {
-        var element = "<div class=\"item\">";
-        for (var j = 0; j < teams[i].length; j++) {
-            element = element.concat("<p>").concat(teams[i][j]).concat("</p>");
+    for (let i = 0; i < teams.length; i++) {
+        const element = document.createElement("div");
+        element.setAttribute("class", "item");
+        if (animation.checked) {
+            element.setAttribute("style", "animation-name: appear; animation-duration: 1s; animation-delay: " + i * 0.1 + "s; opacity: 0%;");
         }
-        container.innerHTML = container.innerHTML.concat(element).concat("</div>");
+        for (let j = 0; j < teams[i].length; j++) {
+            var child = document.createElement("p");
+            child.innerHTML = teams[i][j];
+            element.appendChild(child);
+        }
+        if (animation.checked) {
+            setTimeout(e => {
+                e.style.opacity = "100%";
+            }, i * 100 + 500, element);
+        }
+        container.appendChild(element);
     }
-});
+    if (animation.checked) {
+        chang.play();
+    }
+}
