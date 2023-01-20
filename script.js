91456d4d0ee4d7de17cb9990b1c2c8bf528671c6
Rokr0k <yangeh2225@gmail.com>
2023-01-20 22:46:03 +0900

    feat: massive change go brrrrr

--- script.js
+++ script.js
@@ -1,104 +1,159 @@
-var start = document.getElementById("start");
-const length = document.getElementById("length");
-const except = document.getElementById("except");
-const count = document.getElementById("count");
-const run = document.getElementById("run");
-const save = document.getElementById("save");
-const container = document.getElementById("container");
-const animation = document.getElementById("animation");
+'use strict';
 
-let teamsObj = null;
+/** @typedef {{name: string, score: number}} Member */
+/** @typedef {{members: number[], score: number}} Team */
 
-let tee = null;
-let ii = 0;
-let previousTeam = null;
-let previousIndex = null;
-let previousTimeout = 0;
+const lengthInput = document.querySelector('#length');
+const exceptInput = document.querySelector('#except');
+const countInput = document.querySelector('#count');
+const runInput = document.querySelector('#run');
+const updateInput = document.querySelector('#update');
+const saveInput = document.querySelector('#save');
+const membersInput = document.querySelector('#members');
+const tableElement = document.querySelector('#score-table tbody');
+const containerElement = document.querySelector('#container');
 
-run.addEventListener("click", () => {
-    if (animation.checked) {
-        run.disabled = true;
-        switch (Math.floor(Math.random() * 2)) {
-            case 0:
-                genshin_wish.removeAttribute("hidden");
-                genshin_wish.play();
-                break;
-            case 1:
-                mingrifangzhou_gacha.removeAttribute("hidden");
-                mingrifangzhou_gacha.play();
-                break;
-        }
+/** @type {{[key: number]: Member}} */
+const membersData = {};
+/** @type {Team[]?} */
+let teams = null;
+
+runInput.addEventListener('click', () => roll());
+
+updateInput.addEventListener('click', () => {
+    if (!teams) {
+        return;
     }
-    else {
-        roll().then(teams => work(teams));
+
+    const maxScore = Math.max(...teams.map(team => team.score));
+    if (maxScore == 0) {
+        alert('점수가 없음.');
+        return;
+    }
+
+    const maxTeams = [...teams.keys()].filter(team => teams[team].score >= maxScore);
+    if (confirm(`${maxTeams.map(team => team + 1).join(', ')} 팀에 1점 증가`)) {
+        maxTeams.map(team => teams[team].members).reduce((members, member) => [...members, ...member], []).forEach(member => {
+            if (membersData[member]) {
+                membersData[member].score++;
+            }
+        });
+        updateMembers();
     }
 });
 
-const roll = () => {
-    return new Promise(resolve => {
-        const array = new Array();
-        const exceptions = except.value.split(" ").map(v => +v).filter(v => typeof (v) == "number");
-        for (let i = +start.value; i < +start.value + +length.value; i++) {
-            let done = false;
-            for (let j = 0; j < exceptions.length; j++) {
-                if (i == exceptions[j]) {
-                    done = true;
-                    break;
-                }
-            }
-            if (!done) {
-                array.push(i);
-            }
-        }
-        const teams = new Array(+count.value);
-        for (let i = 0; i < +count.value; i++) {
-            teams[i] = new Array();
-        }
-        let team = 0;
-        while (array.length > 0) {
-            const index = Math.floor(Math.random() * array.length);
-            teams[team].push(array[index]);
-            team = (team + 1) % (+count.value);
-            array.splice(index, 1);
-        }
-        for (let i = 0; i < teams.length; i++) {
-            teams[i] = teams[i].sort((a, b) => a - b);
-        }
-        resolve(teams);
-    })
-}
-
-const work = (teams) => {
-    container.innerHTML = "";
-    for (let i = 0; i < teams.length; i++) {
-        const element = document.createElement("div");
-        element.setAttribute("class", "item");
-        if (animation.checked) {
-            element.setAttribute("style", "animation-name: appear; animation-duration: 1s; animation-delay: " + i * 0.1 + "s; opacity: 0%;");
-        }
-        for (let j = 0; j < teams[i].length; j++) {
-            var child = document.createElement("p");
-            child.innerHTML = teams[i][j];
-            element.appendChild(child);
-        }
-        if (animation.checked) {
-            setTimeout(e => {
-                e.style.opacity = "100%";
-            }, i * 100 + 500, element);
-        }
-        container.appendChild(element);
+/**
+ * Generate teams and update
+ */
+function roll() {
+    const length = +lengthInput.value;
+    const exceptions = exceptInput.value.split(' ').map(v => +v).filter(v => typeof (v) == 'number');
+    const array = [...new Array(length).keys()].map(i => i + 1).filter(i => !exceptions.includes(i));
+    teams = new Array(+countInput.value).fill(0).map(() => ({ members: new Array(), score: 0 }));
+    let team = 0;
+    while (array.length > 0) {
+        const index = Math.floor(Math.random() * array.length);
+        teams[team].members.push(...array.splice(index, 1));
+        team = (team + 1) % +countInput.value;
     }
-    teamsObj = teams;
+    teams.forEach(team => team.members.sort((a, b) => a - b));
+    updateTeams();
 }
 
-save.onclick = () => {
-    if (teamsObj) {
-        const keys = Object.keys(teamsObj);
-        const content = [...teamsObj.map(row => keys.map(i => JSON.stringify(row[i], (_key, value) => value == undefined ? '' : value)).join(','))].join('\r\n');
+/**
+ * Update teams display
+ */
+function updateTeams() {
+    containerElement.innerHTML = '';
+    teams.forEach(team => {
+        const element = document.createElement('div');
+        element.classList.add('item');
+
+        const members = document.createElement('div');
+        members.classList.add('members');
+        team.members.forEach(member => {
+            const child = document.createElement('p');
+            child.innerHTML = membersData[member] ? membersData[member].name : member;
+            members.appendChild(child);
+        });
+        element.appendChild(members);
+
+        const controls = document.createElement('div');
+        controls.classList.add('score-controls');
+        const scoreUp = document.createElement('button');
+        scoreUp.classList.add('input', 'button');
+        scoreUp.innerHTML = '&#65291;';
+        scoreUp.addEventListener('click', () => {
+            team.score++;
+            updateTeams(teams);
+        });
+        const scoreDown = document.createElement('button');
+        scoreDown.classList.add('input', 'button');
+        scoreDown.innerHTML = '&#65293;';
+        scoreDown.addEventListener('click', () => {
+            if (team.score > 0) {
+                team.score--;
+                updateTeams(teams);
+            }
+        });
+        controls.appendChild(scoreUp);
+        controls.appendChild(scoreDown);
+        element.appendChild(controls);
+
+        const score = document.createElement('div');
+        score.classList.add('score-display');
+        score.innerHTML = new Array(team.score).fill('&#10026;').join('');
+        element.appendChild(score);
+
+        containerElement.appendChild(element);
+    });
+    teams = teams;
+}
+
+/**
+ * Update members display
+ */
+function updateMembers() {
+    tableElement.innerHTML = Object.values(membersData).map(member => `<tr><td>${member.name}</td><td>${member.score}</td></tr>`).join('');
+}
+
+membersInput.addEventListener('change', () => {
+    Object.keys(membersData).forEach(key => delete membersData[key]);
+    const file = membersInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.addEventListener('load', () => {
+            const data = reader.result.split(/\r?\n/g).map(line => line.split(/[,;\t]/g).map(a => a.trim())).filter(data => data[0].length > 0).map(data => ({ name: data[0], score: +data[1] || 0 }));
+            lengthInput.value = data.length;
+            data.forEach((data, i) => membersData[i + 1] = data);
+            updateMembers();
+            if (teams) {
+                updateTeams();
+            }
+        });
+        reader.readAsText(file);
+    }
+});
+
+saveInput.addEventListener('click', () => {
+    const content = Object.values(membersData).map(member => `${member.name},${member.score}`).join('\r\n');
+    if (showSaveFilePicker) {
+        showSaveFilePicker({
+            excludeAcceptAllOption: true,
+            suggestedName: 'members.csv',
+            types: [
+                {
+                    description: 'CSV file',
+                    accept: { 'text/csv': ['.csv'] }
+                }
+            ]
+        }).then(handler => handler.createWritable()).then(writable => writable.write(content).then(() => writable.close())).catch(() => { });
+    } else {
         const blob = new Blob([content], { type: 'text/csv' });
-        const anchor = document.createElement("a");
-        anchor.download = "teams.csv";
+        const anchor = document.createElement('a');
+        anchor.download = prompt('파일 이름') || 'members.csv';
         anchor.href = URL.createObjectURL(blob);
         anchor.click();
+        URL.revokeObjectURL(anchor.href);
     }
-}
+});
